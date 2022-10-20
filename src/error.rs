00d3b340a7f48714bfd9a5bf7969d5c21cf1564a
Alexander Lukens <alexander.lukens1@gmail.com>
2022-10-19 19:00:55 -0700

    setup processor account

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,16 @@
 // program specific errors
+use solana_program::program_error::ProgramError;
+use thiserror::Error;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    /// Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
